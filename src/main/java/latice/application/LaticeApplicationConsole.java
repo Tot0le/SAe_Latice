b23265d8aba7764d7c3579a73d4a79dcb92f6e5e
Laspoujas Anatole <anatole.laspoujas@etu.unilim.fr>
2026-04-06 12:20:58 +0200

    add LaticeApplicationConsole & Console

---
+++ src/main/java/latice/application/LaticeApplicationConsole.java
@@ -0,0 +1,11 @@
+package latice.application;
+
+import latice.gui.Console;
+
+public class LaticeApplicationConsole {
+
+	public static void main(String[] args) {
+		Console.title("-- Bienvenue dans notre magnifique jeu de latice ! -- ");
+	}
+
+}
