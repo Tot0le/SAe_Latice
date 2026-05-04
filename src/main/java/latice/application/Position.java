009e258b6ae589d007424a67779e224cd861f04e
Laspoujas Anatole <anatole.laspoujas@etu.unilim.fr>
2026-05-04 10:28:13 +0200

    add Position class

---
+++ src/main/java/latice/application/Position.java
@@ -0,0 +1,20 @@
+package latice.application;
+
+public class Position {
+	Integer row;
+	Integer column;
+	
+	public Position(Integer row, Integer column) {
+		this.row = row;
+		this.column = column;
+	}
+	
+	public Integer getRow() {
+		return row;
+	}
+	
+	public Integer getColumn() {
+		return column;
+	}
+	
+}
