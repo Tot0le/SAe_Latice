088e55aab49eb59d9dbc1a1048caa2381510a585
Prénom Nom <email@domaine.extension>
2026-05-04 11:03:55 +0200

    Pool-and-rack-class-creation

---
+++ src/main/java/latice/application/Pool.java
@@ -0,0 +1,17 @@
+package latice.application;
+
+public class Pool {
+	private int tilesAmounts;
+
+	
+	// getteur
+	public int getTilesAmounts() {
+		return tilesAmounts;
+	}
+
+		// setteur
+	public void setTilesAmounts(int tilesAmounts) {
+		this.tilesAmounts = tilesAmounts;
+	}
+	
+}
