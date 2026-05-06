994d53ee5954794ff833173ece9a5c749ebbf009
Laspoujas Anatole <anatole.laspoujas@etu.unilim.fr>
2026-05-06 08:36:21 +0200

    feat:add removeTileAt method on GameBoard

--- src/main/java/latice/application/GameBoard.java
+++ src/main/java/latice/application/GameBoard.java
@@ -11,52 +11,53 @@ public class GameBoard {
 		this.tiles = new HashMap<>();
 		this.nbCycle = nbCycle;
 	}
-	
+
 	public Integer getNbCycle() {
 		return nbCycle;
 	}
 
-	
-    public Map<Position, Tile> tiles() {                                                                                                                                                                   
-         return tiles;                                                                                                                                                                                      
-     }                                                                                                                                                                                                      
-                                                                                                                                                                                                            
 
-    public boolean isTileAt(Position position) {                                                                                                                                                           
-        return tiles.containsKey(position);                                                                                                                                                                
-     }                                                                                                                                                                                                      
-                                                                                                                                                                                                            
-     public boolean isEmpty() {                                                                                                                                                                             
-         return tiles.isEmpty();                                                                                                                                                                            
-    }                                                                                                                                                                                                      
-                                                                                                                                                                                                            
-     public boolean put(Position position, Tile disc) {                                                                                                                                                     
-         if (! this.isTileAt(position)) {                                                                                                                                                                   
-             this.tiles.put(position, disc);                                                                                                                                                                
-             return true;                                                                                                                                                                                   
-         }                                                                                                                                                                                                  
-         return false;                                                                                                                                                                                      
-     }                                                                                                                                                                                                      
-                                                                                                                                                                                                            
-     public void clear() {                                                                                                                                                                                  
-         this.tiles.clear();                                                                                                                                                                                
-     }                                                                                                                                                                                                      
-                                                                                                                                                                                                            
-     public int howManyTilesOnBoard() {                                                                                                                                                                     
-         return this.tiles.size();                                                                                                                                                                          
-     }                                                                                                                                                                                                      
-                                                                                                                                                                                                            
-     public Tile discAt(Position position) {                                                                                                                                                                
-         return this.tiles.get(position);                                                                                                                                                                   
-     }                                                                                                                                                                                                      
-                                                                                                                                                                                                            
-     public Tile removeTileAt(Position position) {                                                                                                                                                          
-         return null;                                                                                                                                                                                       
-         // TODO : one line instruction : see in javadoc ;-)                                                                                                                                                
-     }                                                                                                                                                                                                      
-                                                                                                                                                                                                            
+    public Map<Position, Tile> tiles() {
+         return tiles;
+     }
+
+
+    public boolean isTileAt(Position position) {
+        return tiles.containsKey(position);
+     }
+
+     public boolean isEmpty() {
+         return tiles.isEmpty();
+    }
+
+     public boolean put(Position position, Tile disc) {
+         if (! this.isTileAt(position)) {
+             this.tiles.put(position, disc);
+             return true;
+         }
+         return false;
+     }
+
+     public void clear() {
+         this.tiles.clear();
+     }
+
+     public int howManyTilesOnBoard() {
+         return this.tiles.size();
+     }
+
+     public Tile discAt(Position position) {
+         return this.tiles.get(position);
+     }
+
+     public Tile removeTileAt(Position position) {
+    	 Tile previousDisc = this.discAt(position);
+ 		 this.tiles.remove(previousDisc);
+ 		 return previousDisc;
+     }
+
      public String toAscii() {                                                                                                                                                                              
-         return "";                                                                                                                                                                                         
-         // TODO                                                                                                                                                                                            
+         return "";
+         // TODO
      }                           
 }
