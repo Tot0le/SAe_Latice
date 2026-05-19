adefa3b6d649cb110d41b065ad5842c9da42f2a5
Loïc Drevet <loic.drevet@etu.unilim.fr>
2026-05-19 17:22:15 +0200

    background image bug_with img which is not found

--- src/main/java/latice/application/LaticeApplication.java
+++ src/main/java/latice/application/LaticeApplication.java
@@ -4,11 +4,21 @@ package latice.application;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+@SuppressWarnings("unused")
 public class LaticeApplication extends javafx.application.Application {
 	
 	public static void main(String[] args) {
@@ -19,21 +29,37 @@ public class LaticeApplication extends javafx.application.Application {
 	public void start(Stage primaryStage) throws Exception {
 		//TODO 
 		//code venant d'un TP (exemple)
-		Group root = new Group();
+		StackPane root = new StackPane();
 		
-		Text txt1 = new Text(50,50,"DRAG ME");
-		Text txt2 = new Text(300,50,"DROP HERE");
+		//Labels
+		Label play = new Label("PLAY");
+		Label quit = new Label("QUIT");
 		
-		Rectangle rectPremier = new Rectangle(150, 100, Color.DARKBLUE);
-		Rectangle rectDeuxieme = new Rectangle(150, 100, Color.DARKGREEN);
-		rectPremier.setX(50);
-		rectPremier.setY(100);
-		rectDeuxieme.setX(300);
-		rectDeuxieme.setY(200);
-		root.getChildren().addAll(txt1,txt2, rectPremier, rectDeuxieme);
-		Scene scene = new Scene(root,500,350);
-		scene.setFill(Color.LIGHTGREEN);
+		// rectangles 
+		Rectangle rectPlay = new Rectangle(150, 100, Color.DARKBLUE);
+		Rectangle rectQuit = new Rectangle(150, 100, Color.DARKGREEN);
+		rectPlay.setX(50);
+		rectPlay.setY(100);
+		rectQuit.setX(300);
+		rectQuit.setY(200);
+		
+		
+		//image
+		Image image = new Image(getClass().getResource("/latice_background.png").toExternalForm());
+
+		System.out.println(image.isError());
+		
+		BackgroundImage background = new BackgroundImage(image, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,BackgroundPosition.CENTER, new BackgroundSize(500, 500, false, false, true, false));
+		
+		
+		root.getChildren().addAll(play, quit, rectPlay, rectQuit);
+		
+		Scene scene = new Scene(root,500,500);
+		root.setBackground(new Background(background));
+		
+		// Sélection de la fenetre à afficher 
 		primaryStage.setScene(scene);
+		primaryStage.setTitle("Game starting window");
 		primaryStage.setResizable(false);
 		primaryStage.show();
 	}	
