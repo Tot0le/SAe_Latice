6b5d627923220dc77f088a1f0932cc3ab46caf20
Laspoujas Anatole <anatole.laspoujas@etu.unilim.fr>
2026-05-27 09:58:10 +0200

    refacto: add StandartPool class and use

--- src/main/java/latice/gui/view/GameScene.java
+++ src/main/java/latice/gui/view/GameScene.java
@@ -15,6 +15,7 @@ import latice.model.Player;
 import latice.model.Pool;
 import latice.model.Rack;
 import latice.model.Referee;
+import latice.model.StandartPool;
 import latice.model.tile.Color;
 import latice.model.tile.Shape;
 import latice.model.tile.Tile;
@@ -53,12 +54,11 @@ public class GameScene {
 	}
 	
 	public void launchGame() {
-		ArrayList<Tile> tiles = Factory.createAllTiles();
-		Pool bigPool = new Pool(tiles); // TODO watch into the standartDeck TP
-		Referee.shuffle(bigPool);
+		Pool standartPool = new StandartPool();
+		Referee.shuffle(standartPool);
 		Pool poolPlayer1 = new Pool();
 		Pool poolPlayer2 = new Pool();
-		Referee.dealTheCards(bigPool, poolPlayer1, poolPlayer2);
+		Referee.dealTheCards(standartPool, poolPlayer1, poolPlayer2);
 		
 		Rack rackPlayer1 = new Rack();
 		Rack rackPlayer2 = new Rack();
