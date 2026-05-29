f50ad3f968870ccdf37df91aac72e5e9a55f5b50
Loïc Drevet <loic.drevet@etu.unilim.fr>
2026-05-29 08:41:05 +0200

    bugFix for the button size

--- src/main/java/latice/gui/view/MainMenu.java
+++ src/main/java/latice/gui/view/MainMenu.java
@@ -32,14 +32,23 @@ public class MainMenu {
 		Image imageQuit = ImageLoader.loadImageSafe(ImagePath.QUIT_BUTTON.imagePath(), 250,250);
 		Image imageRules = ImageLoader.loadImageSafe(ImagePath.RULES_BUTTON.imagePath(),250, 250);
 		
-		//Buttons creation and image attribution 
+		//Buttons creation and image attribution + size min and max
 		Button btnPlay = new Button("PLAY");
+		btnPlay.setPrefSize(150, 100);
+		btnPlay.setMinSize(150, 100);
+		btnPlay.setMaxSize(150, 100);
 		btnPlay = SetImageView.getImageInButton(btnPlay, imagePlay);
 		
-		Button btnQuit = new Button("QUIT"); 
+		Button btnQuit = new Button("QUIT");
+		btnQuit.setPrefSize(150, 100);
+		btnQuit.setMinSize(150, 100);
+		btnQuit.setMaxSize(150, 100);
 		btnQuit = SetImageView.getImageInButton(btnQuit, imageQuit);
 		
 		Button btnRules = new Button("RULES");
+		btnRules.setPrefSize(150, 100);
+		btnRules.setMinSize(150, 100);
+		btnRules.setMaxSize(150, 100);
 		btnRules = SetImageView.getImageInButton(btnRules, imageRules);
 		
 		// Buttons listeners    we make a new primaryStage to bypass the static restrictions
