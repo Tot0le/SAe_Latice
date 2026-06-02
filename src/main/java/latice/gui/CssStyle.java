07db65c12a126eb4a904d70fcac88d875817e8f6
Anatole LASPOUJAS <laspoujasanatole@gmail.com>
2026-06-02 23:24:22 +0200

    big improve on visual in gameScene, needs update of it and needs refacto

--- src/main/java/latice/gui/CssStyle.java
+++ src/main/java/latice/gui/CssStyle.java
@@ -1,10 +1,17 @@
 package latice.gui;
 
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class CssStyle {
-	
+	// Theme constants
+    private static final String themeBlue = "#0C10E3";
+    private static final String themeSoftFawn = "#D1B17C";
+    private static final String themeAntiqueWhite = "#F8E6D2";
+    
 	public static void resultPageCss(Button closeBtn, Button mainBtn, Button replayBtn, VBox middleVbox, VBox topVbox) {
         // Container styles
         middleVbox.setStyle("-fx-background-color: #262421; -fx-background-radius: 12; -fx-border-radius: 12; -fx-border-color: #4A4846; -fx-border-width: 2;");
@@ -41,5 +48,34 @@ public class CssStyle {
         
         closeBtn.setOnMouseEntered(e -> closeBtn.setStyle(closeBtnStyle + " -fx-text-fill: white; -fx-background-color: #E53935; -fx-background-radius: 4;"));
         closeBtn.setOnMouseExited(e -> closeBtn.setStyle(closeBtnStyle));
+    }
+	
+	public static void gameSceneCss(Label currentTurn, Label iconTurn, VBox turnBadge, Label lblPoolTilesNumber, Label iconPool, VBox poolBadge, Label scorePlayer1, Label scorePlayer2, Label currentPlayer, Button exchangeAllTilesBtn, Button buyANewActionBtn, Button endTurnBtn, BorderPane gameLayout, HBox opponentRack, VBox scoreBadge, VBox playerBadge) {
+		// TODO refacto this method into multiple I guess
+		currentTurn.setStyle("-fx-font-size: 28px; -fx-font-weight: bold; -fx-text-fill: " + themeBlue + ";");
+        iconTurn.setStyle("-fx-font-size: 20px;");
+        turnBadge.setStyle("-fx-background-color: " + themeAntiqueWhite + "; -fx-background-radius: 50; -fx-padding: 15; -fx-border-color: " + themeBlue + "; -fx-border-width: 3; -fx-border-radius: 50;");
+        
+        lblPoolTilesNumber.setStyle("-fx-font-size: 26px; -fx-font-weight: bold; -fx-text-fill: " + themeBlue + ";");
+        iconPool.setStyle("-fx-font-size: 22px;");
+        poolBadge.setStyle("-fx-background-color: " + themeAntiqueWhite + "; -fx-background-radius: 12; -fx-padding: 10 20; -fx-border-color: " + themeSoftFawn + "; -fx-border-width: 3; -fx-border-radius: 12;");
+        
+        scorePlayer1.setStyle("-fx-text-fill: " + themeBlue + "; -fx-font-size: 16px; -fx-font-weight: bold;");
+        scorePlayer2.setStyle("-fx-text-fill: " + themeBlue + "; -fx-font-size: 16px; -fx-font-weight: bold;");
+        currentPlayer.setStyle("-fx-text-fill: " + themeBlue + "; -fx-font-weight: bold; -fx-font-size: 18px;");
+                
+        scoreBadge.setStyle("-fx-background-color: " + themeAntiqueWhite + "; -fx-background-radius: 12; -fx-padding: 15; -fx-border-color: " + themeSoftFawn + "; -fx-border-width: 3; -fx-border-radius: 12;");
+        playerBadge.setStyle("-fx-background-color: " + themeAntiqueWhite + "; -fx-background-radius: 12; -fx-padding: 10; -fx-border-color: " + themeBlue + "; -fx-border-width: 3; -fx-border-radius: 12;");
+ 
+        //TODO the constrast is pretty bad, and also add setOnMouseEntered and exited
+        String actionBtnStyle = "-fx-background-color: " + themeSoftFawn + "; -fx-text-fill: " + themeBlue + "; -fx-font-weight: bold; -fx-padding: 8 20; -fx-background-radius: 6; -fx-cursor: hand;";
+        exchangeAllTilesBtn.setStyle(actionBtnStyle);
+        buyANewActionBtn.setStyle(actionBtnStyle);
+        
+        endTurnBtn.setStyle("-fx-background-color: " + themeBlue + "; -fx-text-fill: " + themeAntiqueWhite + "; -fx-font-weight: bold; -fx-padding: 8 30; -fx-background-radius: 6; -fx-cursor: hand;");
+       
+        gameLayout.setStyle("-fx-background-color: linear-gradient(to bottom right, " + themeBlue + ", " + themeSoftFawn + ", " + themeAntiqueWhite + ");");
+
+        opponentRack.setStyle("-fx-background-color: " + themeBlue + "; -fx-background-radius: 8; -fx-border-color: " + themeAntiqueWhite + "; -fx-border-width: 3; -fx-border-radius: 8; -fx-min-width: 450; -fx-max-width: 450; -fx-min-height: 70;");
     }
 }
--- src/main/java/latice/gui/view/GameScene.java
+++ src/main/java/latice/gui/view/GameScene.java
@@ -2,15 +2,18 @@ package latice.gui.view;
 
 import java.util.ArrayList;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
+import latice.gui.CssStyle;
 import latice.gui.controller.GameBtnController;
 import latice.gui.controller.GameController;
 import latice.gui.controller.LabelController;
@@ -40,18 +43,20 @@ public class GameScene extends Scene { //TODO add a resign button
 //	private Button exchangeTilesBtn;
 	private Button confirmBtn;
 	private Button buyANewActionBtn;
-	private ArrayList<Label> scoreLabels;
+	private ArrayList<Label> scoreLabels; //TODO if the name is to long do something (the score is unreadable)
 	private ArrayList<String> usernames;
-	private Label currentPlayer;
+	private Label currentPlayerLbl;
 	private Label endMessageLbl;
-	private Label currentTurn;
+	private Label currentTurnLbl;  // TODO first currentTurnLbl update
 	private Label lblPoolTilesNumber;
+	
+	private static final double SIDE_PANEL_WIDTH = 350.0;
 	
 	public GameScene(ArrayList<String> usernames, MenuBtnController menuBtnController) {
 		super(new StackPane(), 1920, 1080);
 		
-		this.currentTurn = new Label("Turn : 1");
-		this.lblPoolTilesNumber = new Label("Pool : ");
+		this.currentTurnLbl = new Label("1");
+		this.lblPoolTilesNumber = new Label("");
 		this.usernames = usernames;
 		this.gameboard = new GameBoard();
 		this.visualGameboard = new GameBoardIhm(this.gameboard);
@@ -73,21 +78,58 @@ public class GameScene extends Scene { //TODO add a resign button
 		
 		BorderPane.setAlignment(visualGameboard, Pos.CENTER);
 		VBox vboxTop = new VBox();
-		endTurnBtn = new Button("End Turn");
 		
-		endMessageLbl = new Label("");
-
-		vboxTop.getChildren().add(endMessageLbl);
+		// decorative opponent rack
+		HBox opponentRack = new HBox();
 		
+		vboxTop.getChildren().addAll(opponentRack);
+		vboxTop.setAlignment(Pos.TOP_CENTER);
+		vboxTop.setPadding(new Insets(20, 10, 10, 10));
+		
+		vboxTop.setSpacing(15);
+		
+		// right panel setup (scores and currentPlayer)
 		Label scorePlayer1 = new Label("Score " + usernames.get(0) + " : 0");
 		Label scorePlayer2 = new Label("Score " + usernames.get(1) + " : 0");
+		currentPlayerLbl = new Label();
 		
-		currentPlayer = new Label();
+//		scorePlayer1.setWrapText(true);
+		scorePlayer1.setTooltip(new javafx.scene.control.Tooltip("Score " + usernames.get(0) + " : 0")); //TODO modify them when score change, also 1/2 chance that Player 1 & player2 are reversed
+		scorePlayer2.setTooltip(new javafx.scene.control.Tooltip("Score " + usernames.get(1) + " : 0"));
+		currentPlayerLbl.setWrapText(true);
+		
 		VBox vboxRight = new VBox();
-		vboxRight.getChildren().addAll(scorePlayer1, scorePlayer2, currentPlayer);
+		vboxRight.setPrefWidth(SIDE_PANEL_WIDTH);
+		vboxRight.setAlignment(Pos.TOP_CENTER);
+		vboxRight.setPadding(new Insets(30));
+		vboxRight.setSpacing(15);
+		
+		// containers for the score and player rectangle background
+		VBox scoreBadge = new VBox(scorePlayer1, scorePlayer2);
+		scoreBadge.setAlignment(Pos.CENTER);
+		scoreBadge.setSpacing(10);
+		
+		VBox playerBadge = new VBox(currentPlayerLbl);
+		playerBadge.setAlignment(Pos.CENTER);
+		
+		vboxRight.getChildren().addAll(scoreBadge, playerBadge);
+		
+		// left panel setup (turns and Pool badges)
+		Label iconTurn = new Label("⏳");
+		VBox turnBadge = new VBox(iconTurn, currentTurnLbl);
+		turnBadge.setAlignment(Pos.CENTER);
+		turnBadge.setMaxWidth(80);
+		
+		Label iconPool = new Label("🀄"); // TODO change this emoji into a better tile (it's a majong tile)
+		VBox poolBadge = new VBox(iconPool, lblPoolTilesNumber);
+		poolBadge.setAlignment(Pos.CENTER);
 		
 		vBoxLeft = new VBox();
-		vBoxLeft.getChildren().addAll(currentTurn, lblPoolTilesNumber);
+		vBoxLeft.setPrefWidth(SIDE_PANEL_WIDTH);
+		vBoxLeft.setAlignment(Pos.TOP_CENTER);
+		vBoxLeft.setPadding(new Insets(30));
+		vBoxLeft.setSpacing(30);
+		vBoxLeft.getChildren().addAll(turnBadge);
 		
 		scoreLabels = new ArrayList<>();
 		scoreLabels.add(scorePlayer1);
@@ -101,24 +143,42 @@ public class GameScene extends Scene { //TODO add a resign button
 		
 		gameLayout.setCenter(visualGameboard);
 		
+		endTurnBtn = new Button("End Turn");
 		exchangeAllTilesBtn = new Button("Exchange All Tiles");
 		buyANewActionBtn = new Button("Buy an action.");
 		
 		buyANewActionBtn.setDisable(true);
 		
-		hBoxBottom.getChildren().add(exchangeAllTilesBtn);
-		hBoxBottom.getChildren().add(buyANewActionBtn);
+		hBoxBottom.getChildren().addAll(exchangeAllTilesBtn, buyANewActionBtn);
 		hBoxBottom.setAlignment(Pos.CENTER);
-		hBoxBottom.setSpacing(100);
-
-		vBoxBottom.getChildren().add(endTurnBtn);
-		vBoxBottom.getChildren().add(hBoxBottom);
-		vBoxBottom.getChildren().add(rackIhmEmplacement);
-		vBoxBottom.setAlignment(Pos.CENTER);
-		vBoxBottom.setSpacing(10);
+		hBoxBottom.setSpacing(50);
 		
-		//gridPaneBottom.setAlignment(Pos.TOP_CENTER);
-		//gridPaneBottom.setSpacing(10);
+		// Layout bottom elements with AnchorPane to keep rack centered and pool on right
+		AnchorPane rackAndPoolPane = new AnchorPane();
+		HBox rackCenterContainer = new HBox(rackIhmEmplacement);
+		rackCenterContainer.setAlignment(Pos.CENTER);
+		
+		// force the rackCenterContainer HBox to extends itself to the maximum to the left and to the right
+		AnchorPane.setLeftAnchor(rackCenterContainer, 0.0);
+		AnchorPane.setRightAnchor(rackCenterContainer, 0.0);
+		
+		// place the anchor top to it's top
+		AnchorPane.setTopAnchor(rackCenterContainer, 0.0);
+		// place the rack 20 pixels higher to form a small 20px margin bottom
+		AnchorPane.setBottomAnchor(rackCenterContainer, 20.0);
+		
+		// place the poolBadge to be 100 pixels to the left relative to the right & to be 30 pixels up relative to the bottom
+		AnchorPane.setRightAnchor(poolBadge, 100.0);
+		AnchorPane.setBottomAnchor(poolBadge, 30.0);
+		
+		rackAndPoolPane.getChildren().addAll(rackCenterContainer, poolBadge);
+		
+		vBoxBottom.getChildren().addAll(endTurnBtn, hBoxBottom, rackAndPoolPane);
+		vBoxBottom.setAlignment(Pos.CENTER);
+		vBoxBottom.setSpacing(15);
+		
+		// apply CSS
+		CssStyle.gameSceneCss(currentTurnLbl, iconTurn, turnBadge, lblPoolTilesNumber, iconPool, poolBadge, scorePlayer1, scorePlayer2, currentPlayerLbl, exchangeAllTilesBtn, buyANewActionBtn, endTurnBtn, gameLayout, opponentRack, scoreBadge, playerBadge);
 
 		gameLayout.setBottom(vBoxBottom);
 		
@@ -149,7 +209,7 @@ public class GameScene extends Scene { //TODO add a resign button
 		// The game can now start
 		
 		Referee referee = new Referee(gameboard, playerList);
-		LabelController lblController = new LabelController(referee, scoreLabels, currentPlayer, endMessageLbl, lblPoolTilesNumber, currentTurn);
+		LabelController lblController = new LabelController(referee, scoreLabels, currentPlayerLbl, endMessageLbl, lblPoolTilesNumber, currentTurnLbl);
 		gameBtnController = new GameBtnController(referee, exchangeAllTilesBtn, buyANewActionBtn, endTurnBtn);
 		GameController gameController = new GameController(this.gameboard, this.visualGameboard, referee, this, lblController, menuBtnController, gameBtnController);
 		
