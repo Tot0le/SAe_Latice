fca9d1901d3c5557c89f57a501e73f105b27154e
Loïc Drevet <loic.drevet@etu.unilim.fr>
2026-06-04 08:38:54 +0200

    clean refacto

--- src/main/java/latice/model/Position.java
+++ src/main/java/latice/model/Position.java
@@ -31,22 +31,7 @@ public class Position {
 		 
 		return nearbyPositions;
     }
-	
-//	public void IndexPositions() {
-//		for(int i=1; i<10; i++) {
-//			for(int j=1;j<10;j++) {
-//				new Position(i, j);
-//			}
-//		}
-//	}
-//
-//	public boolean PositionExists(Position coords) {
-//		for(int i=1; i<10; i++) {
-//			for(int j=1;j<10;j++) {
-//				if(coords == Position(i, j)) return true;
-//			}
-//		}
-//	}
+
 
 	@Override
 	public boolean equals(Object obj) {
